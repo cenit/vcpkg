d49fce9302a25d4f88fef403947450aa18328975
Stefano Sinigardi <stesinigardi@hotmail.com>
2022-09-19 16:49:25 +0200

    [aravis] bump version and move to manifest file

--- ports/aravis/portfile.cmake
+++ ports/aravis/portfile.cmake
@@ -7,8 +7,8 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO AravisProject/aravis
-    REF e49cfe86dfa5e526de1047db876315340cefce2b
-    SHA512 1b93197031f1a4912911c320e88eda67f7c08db1570c22213f5f2ec41f17e17b6f04c513b8990871e07864137da3cf725389da2aceca4035303560f472ea2a83
+    REF fe36369e0416fe22b3df0fd35e66918f7eea202c
+    SHA512 585479529282e72d8515d46d5d0be3783c5bad57481f8b4f8bcbf702120280149f4c5dea41330225ec00243ff7ab0e411d43312ae7e74142b788d3e45270cb5c
     HEAD_REF master
     PATCHES
         ${REQUIRED_PATCH}
@@ -19,22 +19,7 @@ vcpkg_find_acquire_program(PYTHON3)
 get_filename_component(PYTHON3_DIR "${PYTHON3}" DIRECTORY)
 vcpkg_add_to_path("${PYTHON3_DIR}")
 
-if(CMAKE_HOST_WIN32)
-    if(NOT EXISTS ${PYTHON3_DIR}/easy_install${VCPKG_TARGET_EXECUTABLE_SUFFIX})
-        if(NOT EXISTS ${PYTHON3_DIR}/Scripts/pip${VCPKG_TARGET_EXECUTABLE_SUFFIX})
-            get_filename_component(PYTHON3_DIR_NAME "${PYTHON3_DIR}" NAME)
-            vcpkg_download_distfile(GET_PIP
-                URLS "https://bootstrap.pypa.io/3.3/get-pip.py"
-                FILENAME "tools/python/${PYTHON3_DIR_NAME}/get-pip.py"
-                SHA512 92e68525830bb23955a31cb19ebc3021ef16b6337eab83d5db2961b791283d2867207545faf83635f6027f2f7b7f8fee2c85f2cfd8e8267df25406474571c741
-            )
-            execute_process(COMMAND ${PYTHON3_DIR}/python${VCPKG_TARGET_EXECUTABLE_SUFFIX} ${GET_PIP})
-        endif()
-        execute_process(COMMAND ${PYTHON3_DIR}/Scripts/pip${VCPKG_TARGET_EXECUTABLE_SUFFIX} install python-gettext --user)
-    else()
-        execute_process(COMMAND ${PYTHON3_DIR}/easy_install${VCPKG_TARGET_EXECUTABLE_SUFFIX} python-gettext)
-    endif()
-endif()
+x_vcpkg_get_python_packages(PYTHON_VERSION "3" PACKAGES python-gettext OUT_PYTHON_VAR "PYTHON3")
 
 vcpkg_add_to_path("${CURRENT_INSTALLED_DIR}/tools/gettext")
 vcpkg_add_to_path("${CURRENT_INSTALLED_DIR}/tools/glib")
